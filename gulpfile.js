5f9cdcd67125b11fada8e4f9307a1244a2444c78
Tomio Ueda <tomio@radiantllama.com>
2016-06-24 13:32:46 -0400

    Initial setup and convertion to ES6

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    sass        = require('gulp-sass'),
+    // coffee      = require('gulp-coffee'),
+    babel       = require('gulp-babel'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    autoprefixer= require('gulp-autoprefixer'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    cleanCSS    = require('gulp-clean-css'),
+    uglify      = require('gulp-uglify'),
+    minimist    = require('minimist'),
+    gls         = require('gulp-live-server');
+
+options = minimist(process.argv.slice(2), {
+  default: {
+    env: process.env.NODE_ENV || 'development',
+    port: 3000,
+    host: "0.0.0.0"
+  },
+  string: ["env"],
+  alias: {
+    port: ['p'],
+    host: ['h']
+  }
+});
+
+gulp.task('build-css', function() {
+  return gulp.src(['lib/**/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 10']
+    }))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write({
+      includeContent: false,
+      sourceRoot: 'build/sass'
+    }))
+    .pipe(gulp.dest('server/public/cslib'))
+});
+
+gulp.task('build-js', function() {
+  return gulp.src(['lib/**/*.js', '!lib/thirdparty/**/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015', 'react']
+    }))
+    // .pipe(uglify())
+    .pipe(sourcemaps.write({
+      includeContent: false,
+      sourceRoot: 'build/js'
+    }))
+    .pipe(gulp.dest('server/public/cslib'))
+});
+
+gulp.task('copy-static', function() {
+  gulp.src(['lib/index.html'])
+    .pipe(gulp.dest('server/public'));
+  gulp.src(['lib/modules/**/*.html', 'lib/modules/**/*.jpg', 'lib/modules/**/*.png', 'lib/modules/**/*.gif' ])
+    .pipe(gulp.dest('server/public/cslib/modules'));
+  gulp.src(['lib/thirdparty/**/*'])
+    .pipe(gulp.dest('server/public/cslib/thirdparty'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('lib/**/*.scss', ['build-css']);
+  gulp.watch('lib/**/*.js', ['build-js']);
+});
+
+gulp.task('up', function() {
+  var opts = {
+    cwd: undefined,
+    env: process.env
+  }
+  opts.env.NODE_ENV = options.env;
+  opts.env.PORT = options.port;
+  opts.env.HOST = options.host;
+  
+  var server = gls('./index.js', opts);
+  server.start();
+  gulp.watch(['server/public/**/*.css', 'server/public/**/*.html', 'server/public/**/*.js'], function(file) {
+    server.notify.apply(server, [file]);
+  });
+  gulp.watch('./index.js', server.start.bind(server));
+});
+
+gulp.task('default', ['build-css', 'build-js', 'copy-static', 'watch', 'up']);
+

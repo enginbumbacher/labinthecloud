0a3154ecf50531ea9a9f5d59ad3a05fe74ca8031
Tomio Ueda <tomio@radiantllama.com>
2017-04-19 16:55:20 -0400

    Normalizes data for both model and live results; adjusts results displays accordingly

--- common/euglenaModels/oneEye.js
+++ common/euglenaModels/oneEye.js
@@ -12,11 +12,11 @@ module.exports = {
   },
 
   update: (config) => {
-    const v_eye = EuglenaUtils.transform_roll_pitch_yaw(config.last.roll, 0, config.last.angle, { x: 0, y: 1, z: 0 });
-    const v_head = EuglenaUtils.transform_roll_pitch_yaw(config.last.roll, 0, config.last.angle, { x: 1, y: 0, z: 0 });
+    const v_eye = EuglenaUtils.transform_roll_pitch_yaw(config.last.roll, 0, config.last.yaw, { x: 0, y: 1, z: 0 });
+    const v_head = EuglenaUtils.transform_roll_pitch_yaw(config.last.roll, 0, config.last.yaw, { x: 1, y: 0, z: 0 });
 
     let intensity = 0;
-    let net_angle = 0;
+    let net_yaw = 0;
     for (let k in config.lights) {
       let v_light = { x: 0, y: 0, z: 0 };
       switch (k) {
@@ -39,19 +39,19 @@ module.exports = {
       if (Math.cos(intensity_theta) >= 0 && intensity_light > 0) {
         let parity = Math.sign(v_head.x * v_light.y - v_head.y * v_light.x);
         intensity += intensity_light;
-        net_angle += intensity_light * parity;
+        net_yaw += intensity_light * parity;
       }
     }
-    intensity *= net_angle > 0 ? 1 : -1;
+    intensity *= net_yaw > 0 ? 1 : -1;
 
     const dT = 1 / config.result.fps;
     const out = {
       time: config.frame * dT,
-      angle: config.last.angle + (config.track.oneEye.k * intensity + (Math.random() * 2 - 1) * config.model.configuration.randomness * Math.PI) * dT,
+      yaw: config.last.yaw + (config.track.oneEye.k * intensity + (Math.random() * 2 - 1) * config.model.configuration.randomness * Math.PI) * dT,
       roll: config.last.roll + config.track.oneEye.omega * dT
     }
-    out.x = config.last.x + Math.cos(out.angle) * config.track.oneEye.v * dT
-    out.y = config.last.y + Math.sin(out.angle) * config.track.oneEye.v * dT
+    out.x = config.last.x + Math.cos(out.yaw) * config.track.oneEye.v * dT
+    out.y = config.last.y + Math.sin(out.yaw) * config.track.oneEye.v * dT
 
     return out;
   }

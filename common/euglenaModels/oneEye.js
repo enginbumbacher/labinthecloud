95e3ee4a000a56efa124f62658ad1423bc9e4b36
enginbumbacher <Engin@gse7314162.local>
2017-06-06 21:25:19 -0700

    Fixed 1-Eye Model

--- common/euglenaModels/oneEye.js
+++ common/euglenaModels/oneEye.js
@@ -70,17 +70,18 @@ module.exports = {
     var delta_yaw = (config.track.oneEye.k * intensity) * dT + (Math.random() * 2 - 1) * config.model.configuration.randomness * Math.PI * dT; // Randomize
     const delta_roll = config.track.oneEye.omega * dT;
 
-    const yaw_min = 0.005; //config.params.k / 20.0; // restrict the minimum possible yaw rotation to 0.01 instead of 0
-    if (delta_yaw==0) {delta_yaw = yaw_min;}
+    const yaw_min = 0.05; //config.params.k / 20.0; // restrict the minimum possible yaw rotation to 0.01 instead of 0
+    if (Math.abs(delta_yaw)<0.1) {delta_yaw = yaw_min;}
 
     // Translate forward in head direction *** REMINDER: local z axis is pointing "forward", i.e. in getWorldDirection()
+    //tmp_euglena.translateZ(config.track.oneEye.v * dT);
     tmp_euglena.translateZ(config.track.oneEye.v * dT);
 
     // Roll around the local z-axis (i.e. head)
     tmp_euglena.rotateZ(delta_roll);
 
     // Yaw around the local x-axis, which is orthogonal to the head-eye plane (spanned by the local z and y axes).
-    tmp_euglena.rotateX(delta_yaw);
+    tmp_euglena.rotateX(-delta_yaw);
 
     // Restrict Euglena z position to 0
     tmp_euglena.position.z = 0;

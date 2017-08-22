2601cb1879e07e2b3c169532fd8986a7a28b6d7a
Engin Bumbacher <engin.bumbacher@gmail.com>
2017-08-21 18:54:00 -0700

    Include force logout when maximum time is reached. Disable re-login when forced out.

--- common/models/student.js
+++ common/models/student.js
@@ -15,14 +15,15 @@ module.exports = function(Student) {
           source_id: source_id,
           source: source,
           last_login: (new Date()),
-          last_login_lab: lab
+          last_login_lab: lab,
+          disabled_login: false
         }, function(err, student) {
-          cb(null, student.id, student.source_id)
+          cb(null, student.id, student.source_id, student.disabled_login)
         })
       } else {
         student.updateAttribute('last_login', new Date());
         student.updateAttribute('last_login_lab', lab || null);
-        cb(null, student.id, student.source_id)
+        cb(null, student.id, student.source_id, student.disabled_login)
       }
     })
   };
@@ -44,6 +45,52 @@ module.exports = function(Student) {
     }, {
       'arg': 'source_id',
       'type': 'string'
+    }, {
+      'arg': 'disabled_login',
+      'type': 'boolean'
+    }]
+  });
+
+  Student.logout = function(source_id, student_id, disabled_login, cb) {
+
+    Student.findOne({
+      where: {
+        source_id: source_id,
+        id: student_id
+      }
+    }, function(err, student) {
+      if (err) throw err;
+
+      if (!student) {
+        console.log('we have a problem');
+      } else {
+        console.log(student);
+        student.updateAttribute('disabled_login', disabled_login);
+        cb(null, student.id, student.source_id, student.disabled_login)
+      }
+    })
+  };
+
+  Student.remoteMethod('logout', {
+    accepts: [{
+      arg: 'source_id',
+      type: 'string'
+    }, {
+      arg: 'student_id',
+      type: 'number'
+    }, {
+      arg: 'disabled_login',
+      type: 'string'
+    }],
+    returns: [{
+      'arg': 'student_id',
+      'type': 'number'
+    }, {
+      'arg': 'source_id',
+      'type': 'string'
+    }, {
+      'arg': 'disabled_login',
+      'type': 'boolean'
     }]
   })
 };
